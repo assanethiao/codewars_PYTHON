da2c07c7310667186c2200811e0c055f9e1ea39b
Assane Thiao <132925798+assanethiao@users.noreply.github.com>
2025-09-01 22:21:31 +0200

    Create functionScope.py

---
+++ functionScope.py
@@ -0,0 +1,13 @@
+"""
+Write a function that adds from two invocations.
+
+All inputs will be integers.
+
+add(3)(4)  // 7
+add(12)(20) // 32
+
+"""
+def add(a):
+    def inner(b):
+        return a + b
+    return inner
